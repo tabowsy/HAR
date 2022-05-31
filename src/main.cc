e46a0df11f04ea6fec965dfeacfd066fea40f42d
Oscar Wu <tabowsy@gmail.com>
2022-05-31 11:44:02 +0800

    First commit
    First commit

---
+++ src/main.cc
@@ -0,0 +1,349 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "main_functions.h"
+#include "hx_drv_tflm.h"
+#include "stdio.h"
+
+//#include "synopsys_wei_gpio.h"
+
+// This is the default main used on systems that have the standard C entry
+// point. Other devices (for example FreeRTOS or ESP32) that have different
+// requirements for entry code (like an app_main function) should specialize
+// this main.cc file in a target-specific subfolder.
+// int signal_pass=0;
+// volatile void delay_ms(unsigned int ms);
+// volatile void delay_us(unsigned int ms);
+
+char string_buf[100] = "test\n";
+
+#define accel_scale 1000
+
+typedef struct
+{
+	uint8_t symbol;
+	uint32_t int_part;
+	uint32_t frac_part;
+} accel_type;
+volatile void delay_ms(unsigned int ms)
+{
+  volatile unsigned int delay_i = 0;
+  volatile unsigned int delay_j = 0;
+  volatile const unsigned int delay_cnt = 20000;
+
+  for(delay_i = 0; delay_i < ms; delay_i ++)
+    for(delay_j = 0; delay_j < delay_cnt; delay_j ++);
+} 
+volatile void delay_us(unsigned int us)
+{
+  uint32_t start = 0;
+  uint32_t end = 0;
+  uint32_t diff = 0;
+  hx_drv_tick_get(&start);	
+
+  do{
+	hx_drv_tick_get(&end);	
+	if(end < start){
+		diff = 0XFFFFFFFF - start + end;
+	}else{
+		diff = (end - start);
+	}
+  }while( diff < (us * 400) );
+} 
+
+void GPIO_UART_TX(uint8_t *data, uint32_t len , hx_drv_gpio_config_t *Tx) //gpio0 為板子的Tx
+{
+	Tx -> gpio_data = 0;
+    hx_drv_gpio_set(Tx);
+	for ( int i = 0; i < len; i++ )
+    {
+        uint8_t c = data[i];
+        
+        // pull down as start bit
+		Tx -> gpio_data = 0;
+        hx_drv_gpio_set(Tx);
+        delay_us(26);  // baudrate默認 38400 (26)
+        for ( int j = 0; j < 8; j++ )
+        {
+            if ( c & 0x01 )
+            {
+				Tx->gpio_data = 1;
+                hx_drv_gpio_set(Tx);  //GPIO_PIN_SET = 1
+            }
+            else
+            {
+				Tx->gpio_data = 0;
+                hx_drv_gpio_set(Tx);
+            }
+            delay_us(26);
+            c >>= 1;
+        }
+        // pull high as stop bit
+		Tx->gpio_data = 1;
+        hx_drv_gpio_set(Tx);
+        delay_us(26);
+    }
+}
+void GPIO_UART_RX(uint8_t *data, uint16_t *len, hx_drv_gpio_config_t * Rx) //gpio1 為板子的Rx
+{
+	sprintf(string_buf,"receive\n");
+	hx_drv_uart_print(string_buf);
+	int count = 0; 
+	while(1)
+    {
+        uint8_t b[8] = { 0 };
+        uint32_t c = 0;
+        // wait for start bit
+		do{
+			hx_drv_gpio_get(Rx);
+		}while ( Rx->gpio_data == 1 );  // wait while gpio read high
+        delay_us(26/2);
+		//delay_us(26);
+        for ( int j = 0; j < 8; j++ )
+        {
+            delay_us(26);
+			hx_drv_gpio_get(Rx);
+            b[j] = ( Rx->gpio_data) ? 1 : 0;
+        }
+        
+        for ( int j = 0; j < 8; j++ )
+        {
+            c |= (b[j] << j);
+        }
+        data[count & 0x3fff] = c;
+
+		count++;
+		if(c == 0x0d){   // 0x0d'\r'
+				
+			count |= 0x8000;
+		}else if((c == 0x0a) && (count & 0x8000) ){ //0x0a '\n'
+			count |= 0x4000;
+		}
+		if( count & 0x4000){
+			*len = (count & 0x3fff) - 2;
+			data[*len] = '\0';
+			break;
+		}
+        // wait for stop bit
+		do{
+			hx_drv_gpio_get(Rx);
+		}
+        while ( Rx->gpio_data == 0 );  //wait until gpio read high as stop bit
+		break;
+    }
+	
+	
+}
+static uint16_t sample_data = 51;
+//static uint16_t sample_data = 301;
+static uint16_t sample_data_nim_of_bytes = 17;
+
+int main(int argc, char* argv[])
+{
+	float x_data[3][50];
+	int signal_pass;
+	uint8_t label;
+	int32_t int_buf;
+	accel_type accel_x, accel_y, accel_z;
+	uint32_t msec_cnt = 0;
+	uint32_t sec_cnt = 0;
+	uint16_t counter = 0;
+	uint16_t flag = 0;
+	int32_t flagx,flagy,flagz;
+	uint16_t str_length;
+	hx_drv_gpio_config_t hal_gpio_ZERO;
+	hx_drv_gpio_config_t hal_gpio_ONE;
+	hx_drv_uart_initial(UART_BR_115200);
+
+	//It will initial accelerometer with sampling rate 119 Hz, bandwidth 50 Hz, scale selection 4g at continuous mode.
+	//Accelerometer operates in FIFO mode. 
+	//FIFO size is 32
+
+	hal_gpio_ZERO.gpio_pin = HX_DRV_PGPIO_0;  // 決定pin腳要用數字
+    hal_gpio_ZERO.gpio_direction = HX_DRV_GPIO_OUTPUT;  //HX_DRV_GPIO_OUTPUT(3)
+    hal_gpio_ZERO.gpio_data = 1;
+	
+	if(hx_drv_gpio_initial(&hal_gpio_ZERO) != HX_DRV_LIB_PASS)
+    	hx_drv_uart_print("Accel 0 fail");
+  	else
+    	hx_drv_uart_print("Accel 0 success");
+
+	hal_gpio_ONE.gpio_pin = HX_DRV_PGPIO_1;  // 決定pin腳要用數字
+    hal_gpio_ONE.gpio_direction = HX_DRV_GPIO_INPUT;  //HX_DRV_GPIO_OUTPUT(3)
+    hal_gpio_ONE.gpio_data = 0;
+
+	
+	if(hx_drv_gpio_initial(&hal_gpio_ONE) != HX_DRV_LIB_PASS)
+    	hx_drv_uart_print("Accel 1 fail");
+  	else
+    	hx_drv_uart_print("Accel 1 success");
+
+
+	if (hx_drv_accelerometer_initial() != HX_DRV_LIB_PASS)
+		hx_drv_uart_print("Accelerometer Initialize Fail\n");
+	else
+		hx_drv_uart_print("Accelerometer Initialize Success\n");
+	setup();
+  
+	uint8_t a[sample_data-1][sample_data_nim_of_bytes] ;   
+	uint8_t t[3] = {'1','2','3'};
+	uint8_t in[3];
+	uint16_t count = 3;
+	uint16_t one = 1;
+	uint8_t temp_a[sample_data_nim_of_bytes];
+	uint8_t send_bit;
+	delay_ms(10);
+	if (flag == 0)
+	{
+		GPIO_UART_RX(in,&count,&hal_gpio_ONE);
+		flag +=1;
+	}
+
+	while (1) 
+	{
+
+		float x, y, z;
+		hx_drv_accelerometer_receive(&x, &y, &z);
+
+		x_data[0][counter] = x;
+		x_data[1][counter] = y;
+		x_data[2][counter] = z;
+
+		
+
+		int_buf = x_data[0][counter] * accel_scale; //scale value
+		if(int_buf < 0)
+		{
+			int_buf = int_buf * -1;
+			accel_x.symbol = '-';
+			flagx = -1;
+		}
+		else 
+		{
+			accel_x.symbol = '+';
+			flagx = 1;
+		}
+		accel_x.int_part = int_buf / accel_scale;
+		accel_x.frac_part = int_buf % accel_scale;
+
+
+		int_buf = x_data[1][counter] * accel_scale; //scale value
+		if(int_buf < 0)
+		{
+			int_buf = int_buf * -1;
+			accel_y.symbol = '-';
+			flagy = -1;
+		}
+		else 
+		{
+			accel_y.symbol = '+';
+			flagy = 1;
+		}
+		accel_y.int_part = int_buf / accel_scale;
+		accel_y.frac_part = int_buf % accel_scale;
+
+
+		int_buf = x_data[2][counter] * accel_scale; //scale value
+		if(int_buf < 0)
+		{
+			int_buf = int_buf * -1;
+			accel_z.symbol = '-';
+			flagz = -1;
+		}
+		else 
+		{
+			accel_z.symbol = '+';
+			flagz = 1;
+		}
+		accel_z.int_part = int_buf / accel_scale;
+		accel_z.frac_part = int_buf % accel_scale;
+		sprintf(string_buf, "%d | %c%d.%3d | %c%d.%3d | %c%d.%3d \n", 
+				counter ,accel_x.symbol, accel_x.int_part, accel_x.frac_part, 
+				accel_y.symbol, accel_y.int_part, accel_y.frac_part, 
+				accel_z.symbol, accel_z.int_part, accel_z.frac_part);
+		hx_drv_uart_print(string_buf);
+		temp_a[0] = 0 ; 
+		temp_a[1] = counter >> 8;     //高8位
+		temp_a[2] = counter & 0xff ;  //低8位
+		temp_a[3] = accel_x.symbol == '+';
+		temp_a[4] = accel_x.int_part;
+		temp_a[5] = accel_x.frac_part & 0XFF;
+		temp_a[6] = accel_x.frac_part >> 8;
+		temp_a[7] = accel_y.symbol == '+';
+		temp_a[8] = accel_y.int_part ;
+		temp_a[9] = accel_y.frac_part & 0XFF;
+		temp_a[10] = accel_y.frac_part >> 8;
+		temp_a[11] = accel_z.symbol == '+';
+		temp_a[12] = accel_z.int_part; 
+		temp_a[13] = accel_z.frac_part & 0XFF; 
+		temp_a[14] = accel_z.frac_part >> 8; 
+		temp_a[15] = 0xff; 
+		temp_a[16] = 0xff;
+
+		if (counter == 0)
+		{
+			temp_a[1] = 0x00;
+			temp_a[2] = 0x00;
+		}
+		uint8_t sum = 0;
+		for(int k=1;k<15;k++){
+			sum += temp_a[k];
+		}
+		temp_a[0] = sum;
+		for (int j = 0;j<sample_data_nim_of_bytes;j++)
+		{
+			a[counter][j] = temp_a[j] ;
+			delay_us(10);  
+		}
+
+		
+		msec_cnt = msec_cnt + 125;
+		sec_cnt = sec_cnt + (msec_cnt / 1000);
+		msec_cnt = msec_cnt % 1000;
+
+		GPIO_UART_TX(temp_a,sample_data_nim_of_bytes,&hal_gpio_ZERO);
+
+
+		for(int k=0;k<sample_data_nim_of_bytes;k++){
+			temp_a[k] = 0;   
+		}
+
+		counter++;
+		delay_us(10);
+		if (counter >= 51)
+		{
+
+			loop_har(x_data,&signal_pass);
+			label = signal_pass;
+			GPIO_UART_TX(&label,1,&hal_gpio_ZERO);
+		
+			for (int i = 0 ;i< 50;i++)
+			{	
+				x_data[0][i] = 0;
+				x_data[1][i] = 0;
+				x_data[2][i] = 0;
+			}
+			counter = 0;
+			
+		}
+
+		delay_ms(125);
+	}
+
+	 
+
+
+}
+
---
+++ src/main_functions.cc
@@ -0,0 +1,216 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+#include "hx_drv_tflm.h"
+
+#include "main_functions.h"
+#include "detection_responder.h"
+#include "image_provider.h"
+#include "model_settings.h"
+#include "model.h"
+#include "test_samples.h"
+
+// Globals, used for compatibility with Arduino-style sketches.
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+union float_c{
+  float m_float;
+  uint8_t m_bytes[sizeof(float)];
+};
+union int_c{
+  float m_int;
+  uint8_t m_bytes[sizeof(int)];
+};
+typedef struct
+{
+	uint8_t symbol;
+	uint32_t int_part;
+	uint32_t frac_part;
+} accel_type;
+union float_c myfloat;
+union int_c myint;
+// In order to use optimized tensorflow lite kernels, a signed int8_t quantized
+// model is preferred over the legacy unsigned model format. This means that
+// throughout this project, input images must be converted from unisgned to
+// signed format. The easiest and quickest way to convert from unsigned to
+// signed 8-bit integers is to subtract 128 from the unsigned value to get a
+// signed value.
+
+// An area of memory to use for input, output, and intermediate arrays.
+constexpr int kTensorArenaSize = 200 * 1024;
+alignas(16) uint8_t tensor_arena[kTensorArenaSize] = {0};
+}  // namespace
+
+
+volatile void delay_ms(unsigned int ms);
+
+float find_avg(float list[], int size = 50)
+{
+    float avg;
+    for (int i =0;i<size;i++)
+    {
+      avg += list[i];
+    }
+    return avg / size;
+}
+float find_min(float list[], int size = 50) //'x_min' 'y_min' 'z_min'
+{
+    float min;
+    min = list[0];
+    for (int i = 0;i<size;i++)
+    {
+        if (list[i] < min)
+            min = list[i];
+    }
+    return min;
+}
+
+float find_max(float list[],int size = 50) //'x_max' 'y_max' 'z_max'
+{
+    float max;
+    max = list[0];
+    for (int i = 0;i<size;i++)
+    {
+        if (list[i] > max)
+            max = list[i];
+    }
+    return max;
+}
+float find_maxmin_diff(float list[], int size = 50)
+{
+    float max,min;
+    max = find_max(list);
+    min = find_min(list);
+
+    return max - min;
+}
+
+// The name of this function is important for Arduino compatibility.
+void setup() {
+  //hal_gpio_get(&hal_gpio_0, &gpio_level);
+  // Set up logging. Google style is to avoid globals or statics because of
+  // lifetime uncertainty, but since this has a trivial destructor it's okay.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = tflite::GetModel(model_tflite);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  // Pull in only the operation implementations we need.
+  // This relies on a complete list of all the ops needed by this graph.
+  // An easier approach is to just use the AllOpsResolver, but this will
+  // incur some penalty in code space for op implementations that are not
+  // needed by this graph.
+  //
+  // tflite::AllOpsResolver resolver;
+  // NOLINTNEXTLINE(runtime-global-variables)
+  
+  static tflite::MicroMutableOpResolver<8> micro_op_resolver;
+  micro_op_resolver.AddConv2D();
+  micro_op_resolver.AddMaxPool2D();
+  micro_op_resolver.AddFullyConnected();
+  micro_op_resolver.AddReshape();
+  micro_op_resolver.AddSoftmax();
+  micro_op_resolver.AddConcatenation();
+  micro_op_resolver.AddRelu();
+  micro_op_resolver.AddSplit();
+
+  // Build an interpreter to run the model with.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::MicroInterpreter static_interpreter(
+      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return;
+  }
+
+  // Get information about the memory area to use for the model's input.
+  input = interpreter->input(0);
+  output = interpreter->output(0);
+
+  // Obtain quantization parameters for result dequantization
+
+}
+
+// The name of this function is important for Arduino compatibility.
+void loop_har(float x_data[3][50],int *signal_pass) {
+  char string_buf[100];
+	int32_t test_cnt = 0;
+	int32_t correct_cnt = 0;
+  int32_t int_buf;
+  accel_type accel_x, accel_y, accel_z;
+	float scale = input->params.scale;
+	int32_t zero_point = input->params.zero_point;
+  int32_t temp;
+  int32_t accel_scale = 1000;
+  float x_data_temp[3][50];
+	hx_drv_uart_initial(UART_BR_115200);
+  float avgx,avgy,avgz;
+  float x_diff,y_diff,z_diff;
+  //hal_gpio_set(&hal_gpio_0, GPIO_PIN_RESET); 
+    for (int i = 0 ;i< 50; i++)    //讀取三軸用的迴圈
+    {
+       x_data_temp[0][(i)] = x_data[0][i];
+       x_data_temp[1][(i)] = x_data[1][i];
+       x_data_temp[2][(i)] = x_data[2][i];
+    }
+
+    for (int i = 0; i < 50; i++) {  
+      input->data.f[3*i] = (x_data_temp[0][i] + 4 ) / 8;// x
+      input->data.f[3*i+1] =(x_data_temp[1][i] + 4) / 8;// y
+      input->data.f[3*i+2] = (x_data_temp[2][i] + 4) / 8;// z
+    }
+
+    if (kTfLiteOk != interpreter->Invoke()) {
+      TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed.");
+    }
+    float* results_ptr = output->data.f;
+
+    int result = std::distance(results_ptr, std::max_element(results_ptr, results_ptr + 6));
+
+    float confidence = ((results_ptr[result] - zero_point)*scale + 1) / 2;
+
+    *signal_pass = result;
+
+      TF_LITE_REPORT_ERROR(error_reporter, 
+      "Predicted  %s \n",
+      kCategoryLabels[result]);
+}
+
+
+
